6b7e5a0372f4c59f56cda0b7bcd0dc902b464d4c
Anas7823 <anaselkhiat78@gmail.com>
2024-02-16 16:48:42 +0100

    favicon

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     <title>Login | ArchiDocs</title>
     <link rel="stylesheet" href="./styles/base.css">
     <link rel="stylesheet" href="./styles/index.css">
+    <link rel="shortcut icon" href="./assets/ArchiDocs-Logo.png"/>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://kit.fontawesome.com/3181ebab68.js" crossorigin="anonymous"></script>
 </head>
