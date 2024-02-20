174bca7bd130fc6121e80e0df2dc77ef2d6f29ca
Anas7823 <anaselkhiat78@gmail.com>
2024-02-20 10:04:53 +0100

    nouvelle navbar

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,9 +1,35 @@
+<div class="nav-border">
+    
+    
+    <ul class="nav flex-column">
+        <!-- Logo -->
+        <div class="auth-brand text-center text-lg-start logo-div">
+            <a href="" class="logo">
+                <span><img src="../../assets/ArchiDocs-Logo.png" alt="logo" height="60"></span>
+            </a>
+        </div>
+        <!-- Logo  -->
 
-<ul class="nav-bar">
-    <li class="nav-item"><a href=""><i class="fa-solid fa-folder"></i> Mon espace</a></li>
-    <li class="nav-item"><a href=""><i class="fa-solid fa-hand-holding-heart"></i> Nos offres</a></li>
-    <!-- <li class="nav-item"><a href="">Contact</a></li> -->
-    <li class="nav-item nav-item-right"><a class="active" href=""><i class="fa-solid fa-user"></i> Mon compte</a></li>
-</ul>
+        <li class="nav-item">
+          <a class="nav-link lienNav" href="">Mon espace <i class="bi bi-person-circle"></i></a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link lienNav actif" href=""> ArchiDocs + <i class="bi bi-database-up"></i></a>
+        </li>
+    </ul>
+</div>
 
-
+<!-- Nav hamburger -->
+<div class="nav-hamburger">
+    <div class="btn-hamburger" id="btn-hamburger">
+      <i class="bi bi-list" id="open_nav_hamburger"></i>
+      <i class="bi bi-x-lg" id="close_nav_hamburger"></i>
+    </div>
+    <div class="hamburger">
+      <div class="hamburger__item">
+        <a class="nav-link lienNav" href="">Mon espace <i class="bi bi-person-circle"></i></a>
+        <a class="nav-link lienNav actif" href="">Nos offres <i class="bi bi-database-up"></i></a>
+      </div>
+    </div>
+</div>
+<!-- Nav hambuger -->
--- pages/clients/offres.php
+++ pages/clients/offres.php
@@ -7,9 +7,13 @@
     <title>Offres | ArchiDocs</title>
     <link rel="stylesheet" href="../../styles/base.css">
     <link rel="stylesheet" href="../../styles/offres.css">
+    <script src="../../js/navbar.js" defer></script>
     <link rel="shortcut icon" href="../../assets/ArchiDocs-Logo.png"/>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://kit.fontawesome.com/3181ebab68.js" crossorigin="anonymous"></script>
+    <!-- Icon Bootstrap -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <!-- Icon Bootstrap -->
 </head>
 
 
