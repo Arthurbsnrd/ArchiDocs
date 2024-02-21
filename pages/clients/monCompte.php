24de7b9b2d556b7e35ccc064233c341e79210198
Anas7823 <anaselkhiat78@gmail.com>
2024-02-21 12:44:11 +0100

    avancement dans le front de la page mon compte

--- pages/clients/monCompte.php
+++ pages/clients/monCompte.php
@@ -11,6 +11,8 @@
     <link rel="shortcut icon" href="../../assets/ArchiDocs-Logo.png"/>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://kit.fontawesome.com/3181ebab68.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
     <!-- Icon Bootstrap -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <!-- Icon Bootstrap -->
@@ -47,7 +49,31 @@
             <a href="" class="btn btn-dark  btn-achat-stock btn-look-fich">Consulter mes fichiers</a>
         </div>
         <div class="supp-compte">
+            <h5>Supprimer mon compte</h5>
+            <p>
+                Vous pouvez supprimer votre compte à tout moment, cependant cette action est irréversible. <br>
+                Vous perdrez l'ensemble de vos fichiers stockés sur notre site et ne pourrez plus les récupérer.
+            </p>
+            <button type="button" class="btn btn-danger btn-achat-stock" data-bs-toggle="modal" data-bs-target="#delCompte">Supprimer mon compte</button>
 
+            <div class="modal fade" id="delCompte" tabindex="-1" aria-labelledby="delCompteLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="delCompteLabel">Suppression de compte</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            Êtes-vous sûr de vouloir supprimer votre compte ? <br>
+                            Cette action est irréversible.
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annuler</button>
+                            <a href="" class="btn btn-danger">Supprimer</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </body>
