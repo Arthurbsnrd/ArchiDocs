f88ef5429b4e0cde8c43efabaeecf2924f836417
Anas7823 <anaselkhiat78@gmail.com>
2024-02-22 10:42:30 +0100

    mon espace

--- pages/clients/monEspace.php
+++ pages/clients/monEspace.php
@@ -1,3 +1,30 @@
+<?php
+    $documents = [
+        [
+            "id" => 1,
+            "name" => "Document 1",
+            "type" => "word",
+            "size" => "25mo",
+            "date" => "03/02/2024"
+        ],
+        [
+            "id" => 2,
+            "name" => "Document 2",
+            "type" => "pdf",
+            "size" => "25mo",
+            "date" => "23/03/2024"
+        ],
+        [
+            "id" => 3,
+            "name" => "Document 3",
+            "type" => "excel",
+            "size" => "25mo",
+            "date" => "03/02/2024"
+        ]
+    ];
+    
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
 
@@ -25,57 +52,82 @@
     <div class="contenue monEspace">
         <div class="header-espace">
             <h1>Bienvenue sur votre espace</h1>
-            <p>Retrouvez ici tout vos documents et pourrez en ajouter d'autre</p>
+            <p>Retrouvez ici tout vos documents et ajoutez en d'autre si besoin</p>
         </div>
-        <div class="div-btn-upload">
-            <a href="" class="btn btn-upload">Ajouter un document <i class="fas fa-file-upload"></i></a>
+
+        <div class="stockage">
+            <div class="progress">
+                <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 75%">Utilisation de 15Go</div>
+                Espace restant: 5Go
+            </div>
+            <div class="total-stock">
+                <h5>
+                    Total: 20Go
+                </h5>    
+            </div>
         </div>
+        
         <div class="liste-fichier">
             <div class="les-fichiers">
-                <div class="fichier">
-                    <div class="fichier-img">
-                        <i class="fas fa-file-word"></i>
-                        <small>03/02/2024</small>
+                <div class="div-btn-upload">
+                    <!-- trier les fichier -->
+                    <div class="tri-fichier">
+                        <select name="tri" id="tri" class="btn btn-dark">
+                            <option value="default">Trier par...</option>
+                            <option value="date">Trier par date</option>
+                            <option value="name">Trier par nom</option>
+                            <option value="type">Trier par type</option>
+                        </select>
                     </div>
-                    <div class="fichier-info">
-                        <h3><a href="">Document 1</a></h3>
-                        <p>Document word</p>
-                        <p>Taille: 25mo</p>
-                    </div>
-                    <div class="fichier-action">
-                        <a href="" class="btn btn-dark">Télécharger <i class="bi bi-download"></i></a>
-                        <a href="" class="btn btn-danger">Supprimer <i class="bi bi-trash"></i></a>
-                    </div>
+                    <button data-bs-toggle="modal" data-bs-target="#uploadModal" class="btn btn-upload">Ajouter un document <i class="fas fa-file-upload"></i></button>
                 </div>
-                <div class="fichier">
-                    <div class="fichier-img">
-                        <i class="fas fa-file-pdf"></i>
-                        <small>23/03/2024</small>
+                <?php foreach($documents as $document): ?>
+                    <div class="fichier">
+                        <div class="fichier-img">
+                            <?php if($document["type"] == "word"): ?>
+                                <i class="fas fa-file-word"></i>
+                            <?php elseif($document["type"] == "pdf"): ?>
+                                <i class="fas fa-file-pdf"></i>
+                            <?php elseif($document["type"] == "excel"): ?>
+                                <i class="fas fa-file-excel"></i>
+                            <?php endif; ?>
+                            <small><?= $document["date"] ?></small>
+                        </div>
+                        <div class="fichier-info">
+                            <h3><a href=""><?= $document["name"] ?></a></h3>
+                            <p>Document <?= $document["type"] ?></p>
+                            <p>Taille: <?= $document["size"] ?></p>
+                        </div>
+                        <div class="fichier-action">
+                            <a href="" class="btn btn-dark">Télécharger <i class="bi bi-download"></i></a>
+                            <a href="" class="btn btn-danger">Supprimer <i class="bi bi-trash"></i></a>
+                        </div>
                     </div>
-                    <div class="fichier-info">
-                        <h3><a href="">Document 2</a></h3>
-                        <p>Document pdf</p>
-                        <p>Taille: 25mo</p>
-                    </div>
-                    <div class="fichier-action">
-                        <a href="" class="btn btn-dark">Télécharger <i class="bi bi-download"></i></a>
-                        <a href="" class="btn btn-danger">Supprimer <i class="bi bi-trash"></i></a>
-                    </div>
+                <?php endforeach; ?>
+            </div>
+        </div>
+    </div>
+
+    <!-- Modal -->
+    <div class="modal fade odal-dialog-centered upload-modal" id="uploadModal" tabindex="-1" aria-labelledby="uploadModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="uploadModalLabel">Ajouter un document</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
-                <div class="fichier">
-                    <div class="fichier-img">
-                        <i class="fas fa-file-excel"></i>
-                        <small>03/02/2024</small>
-                    </div>
-                    <div class="fichier-info">
-                        <h3><a href="">Document 3</a></h3>
-                        <p>Document excel</p>
-                        <p>Taille: 25mo</p>
-                    </div>
-                    <div class="fichier-action">
-                        <a href="" class="btn btn-dark">Télécharger <i class="bi bi-download"></i></a>
-                        <a href="" class="btn btn-danger">Supprimer <i class="bi bi-trash"></i></a>
-                    </div>
+                <div class="modal-body upload-form">
+                    <form action="" enctype="multipart/form-data">
+                        <div class="mb-3">
+                            <label for="name" class="form-label">Nom du fichier</label>
+                            <input type="text" class="form-control" id="name" name="name">
+                        </div>
+                        <div class="mb-3">
+                            <label for="file" class="form-label">Choisir un fichier</label>
+                            <input type="file" class="form-control" id="file" name="file">
+                        </div>
+                        <button type="submit" class="btn btn-primary">Ajouter</button>
+                    </form>
                 </div>
             </div>
         </div>
