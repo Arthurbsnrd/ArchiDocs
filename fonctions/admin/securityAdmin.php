18fe1fd22bb240d4a4014c0101de36412ad20262
Anas7823 <anaselkhiat78@gmail.com>
2024-06-25 16:42:28 +0200

    correction sur les accès aux pages admin

--- fonctions/admin/securityAdmin.php
+++ fonctions/admin/securityAdmin.php
@@ -1,7 +1,9 @@
 <?php 
 if(session_status() !== PHP_SESSION_ACTIVE) session_start();
-if(!isset($_SESSION['auth']) && $_SESSION['auth']['role'] != "admin")
+if(!isset($_SESSION['auth']) || $_SESSION['role'] !== "admin")
     {
         header('Location: ../../index.php');
-    }   
+    }   else {
+        print_r($_SESSION);
+    }
 ?>
