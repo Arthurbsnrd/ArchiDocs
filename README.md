d28cf0bc781c475fb575889176d58d2c41757539
Anas7823 <anaselkhiat78@gmail.com>
2024-06-26 20:28:46 +0200

    pdf qui se génère et se range dans le bon dossier lorsque l on va sur le fichier de la fonction

--- README.md
+++ README.md
@@ -1,3 +1,32 @@
-Reste à faire:
+# ArchiDocs
 
-Page monEspace.php -> Faire les fonctions de tri
+Bienvenue sur le projet ArchiDocs, un projet d'application web qui permet aux clients d'un cabinet d'architecte de stocker leurs fichiers et de les gérer. Le projet est basé sur PHP pour le backend et sur HTML, CSS et JavaScript pour le frontend.
+Plusieurs aspect sont abordés dans ce projet notamment la gestion des accès, la sécurité et le bon fonctionnement des fonctionnalités du site.
+
+
+## Démarrage
+
+Pour pouvoir démarrer le projet, vous devez avoir PHP au minimum sur votre ordinateur.
+Ensuite, vous devez récupérer la base de données qui est dans le fichier archidocs.sql.
+Ensuite il suffit de vous inscrire pour avoir un accès et passer votre role à "admin" qui vous voulez avoir tout les droit.
+
+
+## Auteurs
+
+* **EL KHIAT Anas** _alias_ [@Anas7823](https://github.com/Anas7823)
+* **ELOUAKILI Fatima zahra** _alias_ [@fze04](https://github.com/fze04)
+* **BESNARD Arthur** _alias_ [@Arthurbsnrd](https://github.com/Arthurbsnrd)
+
+
+## Erreur possible
+
+Si vous avez une erreur de type: SQLSTATE[HY000] [1045] Access denied for user 'root'@'localhost' (using password: NO)
+Configurer le fichier bdd.php avec les informations de connexion à la base de données.
+
+
+## Comment fonctionne nos PDF ??
+
+Pour générer un PDF de facture, nous utilisons Dompdf.
+Dompdf est un package PHP qui permet de générer des PDF à partir de HTML. Dans notre cas notre squelette est le suivant: factureSquelette.php
+Dans se squelette, nous utilisons les données de l'utilisateur connecté qui fait l'achat puis les affichons dans le PDF.
+Le PDF est ensuite sauvegardé dans le dossier facturesClient.
---
+++ fonctions/clients/factureSquelette.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Facture ArchiDocs</title>
+    <style>
+        .logoHeader {
+            width: 100px;
+            height: 100px;
+            margin-right: 20px;
+        }
+
+        .logoHeader img {
+            width: 120px;
+            height: 100%;
+        }
+
+        .titreHeader {
+            width: 100%;
+            text-align: center;
+        }
+
+        .titreHeader h1 {
+            font-size: 30px;
+        }
+
+        .infosArchidocs {
+            width: 100%;
+            display: flex;
+            flex-direction: column;
+            align-items: end;
+            margin-top: 20px;
+        }
+
+        .infosClient {
+            width: 100%;
+            display: flex;
+            flex-direction: column;
+            margin-top: 20px;
+            float: right;
+        }
+
+        .infosFacture {
+            width: 100%;
+            display: flex;
+            flex-direction: column;
+            border: 1px solid black;
+            border-radius: 10px;
+            padding: 10px;
+            /* margin: 0 auto; */
+        }
+
+        table {
+            width: 100%;
+            table-layout: fixed;
+            border-collapse: collapse;
+        }
+
+        th, td {
+            text-align: center;
+            padding: 8px;
+        }
+
+        th {
+            background-color: #ddd;
+            color: black;
+        }
+    </style>
+</head>
+<body>
+    <div class="infosArchidocs">
+        <h1>Facture ArchiDocs</h1>
+        <h3>ArchiDocs</h3>
+        <h4>Siret : XXXXXXXXXXXXX</h4>
+    </div>
+
+    <div class="infosFacture">
+        <table>
+            <thead>
+                <tr>
+                    <th>Désignation</th>
+                    <th>Numéro de facture</th>
+                    <th>Date</th>
+                    <th>Montant HT unitaire (€)</th>
+                    <th>Montant TTC unitaire (€)</th>
+                    <th>Quantité</th>
+                    <th>TVA</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <tr>
+                    <td>Achat de stockage dans l'espace ArchiDocs</td>
+                    <td><?= $nbFacture[0] + 1?></td>
+                    <td><?= date('Y-m-d')?></td>
+                    <td>16.00</td>
+                    <td>20.00</td>
+                    <td>1</td>
+                    <td>20%</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
+    
+    <div class="infosClient">
+        <h3>Nom : <?= $userInfos['nom'] ?></h3>
+        <h3>Prenom : <?= $userInfos['prenom'] ?></h3>
+        <h3>Téléphone : <?= $userInfos['tel'] ?></h3>
+        <h3>Adresse : <?= $userInfos['adresse'] ?></h3>
+    </div>
+</body>
+
+</html>
